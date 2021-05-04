17cbcaa2a1dde061a7d400f6a4a6fe2a1be2c3e7
Pankaj Chandravanshi <40656326+pankajchandravanshi-gep@users.noreply.github.com>
2021-05-04 23:05:02 +0530

    Update main.tf

--- VMCreation/main.tf
+++ VMCreation/main.tf
@@ -62,44 +62,5 @@ vm_size               = var.config_vm
   }
 }
 
-resource "azurerm_virtual_machine" "azurevm"  {
-name                  = var.name_winvm
-location              = azurerm_resource_group.rg.location
-resource_group_name   = azurerm_resource_group.rg.name
-network_interface_ids = [azurerm_network_interface.nic.id]
-vm_size               = var.config_vm
-  
-  storage_image_reference {
-    
-    publisher   = "MicrosoftWindowsServer"
-    offer       = "WindowsServer" 
-    sku         = var.model_vm
-    version     = "latest"
-    } 
-  
-    storage_os_disk { 
-    name              = "E-Drive"
-    caching           =  var.access_disk    
-    create_option     = "FromImage"
-    managed_disk_type =  var.storage_type
-    }
-  
-    os_profile {
-    computer_name  = "WINDOWSER542021"
-    admin_username =  var.userid
-    admin_password =  var.password
-  }
-
-  os_profile_windows_config {
-    provision_vm_agent = true
-  }
-  
-     tags = {
-    projectName  = var.tag_projectname_vm
-    environment  = var.tag_environment_vm
-    documentTeam = var.tag_department_vm
-    resourceType = var.tag_resource_vm
-  }
-}
 
 
